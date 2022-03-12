0cc1e66c1cf93be2c62be6b79d98451e38a4e72f
gouxinyu <gxy880520@qq.com>
2022-03-12 20:23:01 +0800

    test rotate

--- src/content/App.vue
+++ src/content/App.vue
@@ -4,7 +4,7 @@
         <n-collapse-transition>
             <div class="video-roll-content">
                 <div class="video-roll-website">
-                    <span>视频网站: bilibili</span>
+                    <span>视频网站: {{ webInfo.name }}</span>
                 </div>
                 <div class="video-roll-rotate-control">
                     <n-button
@@ -35,15 +35,47 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, ref, provide } from 'vue';
+import { defineComponent, reactive, ref, onMounted } from 'vue';
 import Head from './Head.vue';
-import { NButton, NSwitch, NIcon, NCollapseTransition } from 'naive-ui'
+import { NButton, NIcon, NCollapseTransition } from 'naive-ui'
 import { ChevronBackOutline } from '@vicons/ionicons5';
 import WEBSITE from '../website';
 
 export default defineComponent({
     name: "App",
     setup(props) {
+        const webInfo = reactive({
+            tabId: '',
+            name: '',
+            videoSelector: []
+        });
+
+        const urlReg = /^http(s)?:\/\/(.*?)\//;
+
+        const getWebInfo = (hostName) => {
+            for (const key of Object.keys(WEBSITE)) {
+                if (hostName.includes(key)) {
+                    const target = WEBSITE[key];
+                    webInfo.name = target.name;
+                    webInfo.videoSelector = target.videoSelector;
+                    return;
+                }
+            }
+            if (!webInfo.name) {
+                webInfo.name = hostName;
+                webInfo.videoSelector = ['video'];
+            }
+        }
+
+        onMounted(async () => {
+            let queryOptions = { active: true, currentWindow: true };
+            let [tab] = await chrome.tabs.query(queryOptions);
+
+            const hostName = urlReg.exec(tab.url)[2];
+            getWebInfo(hostName);
+            webInfo.tabId = tab.id;
+        });
+
         const rotateBtns = ref([
             {
                 type: 'left',
@@ -67,90 +99,21 @@ export default defineComponent({
             }
         ]);
 
-        const getScaleNumber = (dom: HTMLVideoElement, deg: number) => {
-            // get video size
-            const { videoWidth, videoHeight, offsetWidth, offsetHeight } = dom;
-
-            const isHorizonDeg = deg === 90 || deg === 270;
-
-            // 根据原始视频的宽高比例，和容器的宽高比例，计算缩放比例
-            const isHorizonVideo = videoWidth > videoHeight;
-            const isHorizonDom = offsetWidth > offsetHeight;
-
-            // 判断旋转后的缩放比例
-            // 1.若是竖屏视频，但在横屏容器中，初始就是等比缩小的
-            if (isHorizonDeg && !isHorizonVideo && isHorizonDom) {
-                return videoHeight / videoWidth;
-            }
-
-            // 2.若是竖屏视频，横屏中，旋转回0或180
-            if (!isHorizonDeg && !isHorizonVideo && isHorizonDom) {
-                return 1;
-            }
-
-            // 3.若是横屏视频，处在横屏容器中
-            if (isHorizonDeg && isHorizonVideo && isHorizonDom) {
-                return offsetHeight / offsetWidth;
-            }
-
-            if (!isHorizonDeg && isHorizonVideo && isHorizonDom) {
-                return 1;
-            }
-        }
-
         /**
          * 旋转
          */
         const rotate = async (item) => {
             let queryOptions = { active: true, currentWindow: true };
             let [tab] = await chrome.tabs.query(queryOptions);
-            console.log(tab);
-            chrome.tabs.sendMessage(tab.id, { item }, {}, (res) => {
+            chrome.tabs.sendMessage(tab.id, { webInfo, deg: item.deg }, {}, (res) => {
                 console.log(res);
-            })
-            const { deg } = item;
-            const { hostname } = window.location;
-            // console.log(WEBSITE);
-            let website = null;
-            for (const key of Object.keys(WEBSITE)) {
-                if (hostname.includes(key)) {
-                    website = WEBSITE[key];
-                    return;
-                }
-            }
-
-            if (website) {
-                for (const item of website.videoSelector) {
-                    const dom = document.querySelector(item);
-                    if (dom) {
-                        dom.style.transform = `rotate(${deg}deg)`;
-                        return;
-                    }
-                }
-            } else {
-                const dom = document.querySelector('video');
-                if (dom) {
-                    const scale = getScaleNumber(dom, deg);
-                    dom.style.transform = `rotate(${deg}deg) scale(${scale})`;
-                    return;
-                }
-            }
-            // chrome.tabs.query(
-            //     { active: true, currentWindow: true },
-            //     function (tabs) {
-            //         chrome.scripting.executeScript({
-            //             target: { tabId: tabs[0].id },
-            //             function: () => {
-            //                 console.log(123);
-            //             }
-            //         });
-            //     }
-            // );
+            });
         }
 
         return {
             rotateBtns,
-            rotate
+            rotate,
+            webInfo
         }
     },
     components: {
---
+++ src/inject/injected.ts
@@ -0,0 +1,73 @@
+/*
+ * @description: inject
+ * @Author: Gouxinyu
+ * @Date: 2022-01-11 23:49:59
+ */
+
+/**
+ * 计算视频缩放比例
+ * @param dom
+ * @param deg
+ * @returns
+ */
+function getScaleNumber(dom: HTMLVideoElement, deg: number) {
+    // get video size
+    const { videoWidth, videoHeight, offsetWidth, offsetHeight } = dom;
+
+    const isHorizonDeg = deg === 90 || deg === 270;
+
+    // 根据原始视频的宽高比例，和容器的宽高比例，计算缩放比例
+    const isHorizonVideo = videoWidth > videoHeight;
+    const isHorizonDom = offsetWidth > offsetHeight;
+
+    // 判断旋转后的缩放比例
+    // 1.若是竖屏视频，但在横屏容器中，初始就是等比缩小的
+    if (isHorizonDeg && !isHorizonVideo && isHorizonDom) {
+        const scale = videoHeight / videoWidth;
+        // if video element is shadowdom, cant get video height;
+        return Number.isNaN(scale) ? 1 : scale;
+    }
+
+    // 2.若是竖屏视频，横屏中，旋转回0或180
+    if (!isHorizonDeg && !isHorizonVideo && isHorizonDom) {
+        return 1;
+    }
+
+    // 3.若是横屏视频，处在横屏容器中
+    if (isHorizonDeg && isHorizonVideo && isHorizonDom) {
+        return offsetHeight / offsetWidth;
+    }
+
+    if (!isHorizonDeg && isHorizonVideo && isHorizonDom) {
+        return 1;
+    }
+
+    return 1;
+}
+
+/**
+ * 旋转视频
+ * @param deg
+ * @param videoSelector
+ * @returns
+ */
+function rotateVideo(deg: number, videoSelector: string[]) {
+    for (const item of videoSelector) {
+        const dom = document.querySelector(item) as HTMLVideoElement;
+
+        if (!dom) continue;
+
+        if (dom) {
+            const scale = getScaleNumber(dom, deg);
+            dom.style.transform = `rotate(${deg}deg) scale(${scale})`;
+            return;
+        }
+    }
+}
+
+(function () {
+    chrome.runtime.onMessage.addListener((a, b) => {
+        const { webInfo, deg } = a;
+        rotateVideo(deg, webInfo.videoSelector);
+    });
+})();
